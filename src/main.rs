0bb4e19481756edc856b5b6e42e32f4c9a76f9cb
R Wood <archaurore@gmail.com>
2019-11-09 16:31:28 +0100

    finally got #%(*#^&! functions to work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn calculated_average (vec_of_nums : &Vec<usize>, vec_lng : usize) -> usize {
     let mut sum_of_nums:usize = 0;
 
@@ -8,8 +10,20 @@ fn calculated_average (vec_of_nums : &Vec<usize>, vec_lng : usize) -> usize {
     sum_of_nums / vec_lng
 }
 
+fn calculate_mode <'a> (vec_of_nums : &Vec<usize>, num_frequencies : &'a HashMap<usize, i32>, mut highest_freq : Option<&'a i32>) -> Option<&'a i32> {
+    for num in vec_of_nums {
+        let freq = num_frequencies.get(&num);
+
+        if highest_freq < freq {
+            highest_freq = freq;
+        }
+    }
+
+    highest_freq
+}
+
 fn main() {
-    let mut vec_of_nums:Vec<usize> = vec![5, 1, 3, 8, 9];
+    let mut vec_of_nums:Vec<usize> = vec![5, 1, 5, 8, 9];
     let vec_lng:usize = vec_of_nums.len();
 
     let half_index:usize = vec_lng / 2;
@@ -18,7 +32,16 @@ fn main() {
     let median = &vec_of_nums[half_index];
 
     let average = calculated_average(&vec_of_nums, vec_lng);
+    let mut num_frequencies = HashMap::new();
 
+    for &num in &vec_of_nums {
+        let counter = num_frequencies.entry(num).or_insert(0);
+        *counter += 1;
+    }
 
-    println!("The average is {}, the median is {}, the half index is {}", average, median, half_index);
+    let highest_freq:Option<&i32> = Some(&0);
+
+    let mode = calculate_mode(&vec_of_nums, &num_frequencies, highest_freq);
+
+    println!("The average is {}, the median is {}, the mode is {:?}", average, median, mode);
 }
