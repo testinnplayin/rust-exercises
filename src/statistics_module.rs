a8720b3bc233f7ada3f429aeea9d68a21d803a6f
R Wood <archaurore@gmail.com>
2019-11-10 10:14:22 +0100

    refactored a bit in statistics_module

--- src/statistics_module.rs
+++ src/statistics_module.rs
@@ -2,22 +2,28 @@ use std::collections::HashMap;
 
 mod simple_calculators;
 
+fn build_num_frequencies (vec_of_nums : &Vec<usize>) -> HashMap<usize, i32>{
+    let mut num_frequencies = HashMap::new();
+
+    for &num in vec_of_nums {
+        let counter = num_frequencies.entry(num).or_insert(0);
+        *counter += 1;
+    }
+
+    num_frequencies
+}
+
 pub fn trigger_main () {
     let mut vec_of_nums:Vec<usize> = vec![5, 1, 5, 8, 9];
     let vec_lng:usize = vec_of_nums.len();
-
     let half_index:usize = vec_lng / 2;
 
     &vec_of_nums.sort();
     let median = &vec_of_nums[half_index];
 
     let average = simple_calculators::calculate_average(&vec_of_nums, vec_lng);
-    let mut num_frequencies = HashMap::new();
-
-    for &num in &vec_of_nums {
-        let counter = num_frequencies.entry(num).or_insert(0);
-        *counter += 1;
-    }
+    
+    let num_frequencies = build_num_frequencies(&vec_of_nums);
 
     let highest_freq:Option<&i32> = Some(&0);
 
